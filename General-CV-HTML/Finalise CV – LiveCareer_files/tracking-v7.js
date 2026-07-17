cde7388cd4754d95d73bc1126a1774bad6e5c583
Hafizur Rahman <hafiz6512@gmail.com>
2026-07-17 14:25:20 +0600

    Webdeveloper CV HTML

---
+++ General-CV-HTML/Finalise CV – LiveCareer_files/tracking-v7.js
@@ -0,0 +1,81 @@
+var _0x2efb=["toString","exports","call","loaded","Promise","apply","length","next","done","toLowerCase","listeners","emit","_onFulfilled","_umark","push","replace","stack","_next","nextTick","then","object","Symbol","iterator","species","return","_uh","_st","Invalid this","Invalid argument","Chaining cycle detected for promise","Uncaught (in promise)","rejectionHandled","default","prototype","speciesConstructor","resolve","race","reject","all","constructor","unhandledRejection","error","longStack","enableLongStackTrace","_onRejected","dbg_ev_queue","/visitor/v4/visits","https://api-visitor.","/v4/visits","referrer","vssessionuid","hostname","ref","vstr","vsuid","vsutms","location","href","pwb","stringify","VisitorCallNotInitiated","VisitorCallCompleted","error_code","visitRespError","log","visits call returned error","hittype","visit_uid","user_uid","NewVisitor","visitor_uid","getFullYear","toUTCString","ref_id","visitinfo","vsintentcd","CookiesWrittenSuccessfully","version","MetricsUtil","isInitDone","sendDebugDataEvent","VisitorLoopCount","visitor_js_version","sendPageViewEventAfterVisit","ClassInitializers","SendEventProperties","message","setInterval","clearInterval","cookie","match","(^|;)\\s*","\\s*=\\s*([^;]+)","pop","expires=","secure;","XMLHttpRequest","MSXML2.XMLHTTP.3.0","open","POST","setRequestHeader","application/json","content-type","application/json;charset=UTF-8","onreadystatechange","responseText","send","=0;","indexOf","=;expires=","charAt","floor","random","=([^&#]*)","search","utm_source","utm_campaign","utm_term","utm_content","||||","lastIndexOf","localeCompare","split","performance","getEntriesByType","navigation","type","reload","UserStatus","parse","User","UserId","getTime","xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"]
+!function(x,n){!function(n){for(;--n;)x.push(x.shift())}(++n)}(_0x2efb,269)
+var _0x159f=function(x,n){return x-=0,_0x2efb[x]}
+!function(x){function n(e){if(t[e])return t[e][_0x159f("0x0")]
+var f=t[e]={exports:{},id:e,loaded:!1}
+return x[e][_0x159f("0x1")](f[_0x159f("0x0")],f,f[_0x159f("0x0")],n),f[_0x159f("0x2")]=!0,f.exports}var t={}
+n.m=x,n.c=t,n.p="",n(0)}({0:function(x,n,t){(function(x){var n=t(80)
+try{(x||{})[_0x159f("0x3")]=n,window.Promise=n}catch(x){}})[_0x159f("0x1")](n,function(){return this}())},80:function(x,n){(function(n){!function(){"use strict"
+function t(){return fx[A][q]||X}function e(x){return x&&"object"==typeof x}function f(x){return"function"==typeof x}function i(x,n){return x instanceof n}function r(x){return i(x,O)}function o(x,n,t){if(!n(x))throw a(t)}function _(){try{return I[_0x159f("0x4")](k,arguments)}catch(x){return nx.e=x,nx}}function u(x,n){return I=x,k=n,_}function c(x,n){function t(){for(var t=0;t<f;)n(e[t],e[t+1]),e[t++]=U,e[t++]=U
+f=0,e[_0x159f("0x5")]>x&&(e[_0x159f("0x5")]=x)}var e=H(x),f=0
+return function(x,n){e[f++]=x,e[f++]=n,2===f&&fx.nextTick(t)}}function s(x,n){var t,e,r,o,_=0
+if(!x)throw a(G)
+var c=x[fx[A][N]]
+if(f(c))e=c[_0x159f("0x1")](x)
+else{if(!f(x[_0x159f("0x6")])){if(i(x,H)){for(t=x[_0x159f("0x5")];_<t;)n(x[_],_++)
+return _}throw a(G)}e=x}for(;!(r=e[_0x159f("0x6")]())[_0x159f("0x7")];)if((o=u(n)(r.value,_++))===nx)throw f(e[J])&&e[J](),o.e
+return _}function a(x){return new TypeError(x)}function l(x){return(x?"":K)+(new O).stack}function v(x,n){var t="on"+x[_0x159f("0x8")](),e=D[t]
+E&&E[_0x159f("0x9")](x).length?x===xx?E.emit(x,n._v,n):E[_0x159f("0xa")](x,n):e?e({reason:n._v,promise:n}):fx[x](n._v,n)}function d(x){return x&&x._s}function p(x){if(d(x))return new x(tx)
+var n,t,e
+return n=new x(function(x,f){if(n)throw a()
+t=x,e=f}),o(t,f),o(e,f),n}function w(x,n){return function(t){V&&(x[Y]=l(!0)),n===F?b(x,t):T(x,n,t)}}function h(x,n,t,e){return f(t)&&(n[_0x159f("0xb")]=t),f(e)&&(x[z]&&v($,x),n._onRejected=e),V&&(n._p=x),x[x._c++]=n,x._s!==L&&ix(x,n),n}function m(x){if(x._umark)return!0
+x[_0x159f("0xc")]=!0
+for(var n,t=0,e=x._c;t<e;)if(n=x[t++],n._onRejected||m(n))return!0}function y(x,n){function t(x){return e[_0x159f("0xd")](x[_0x159f("0xe")](/^\s+|\s+$/g,""))}var e=[]
+return V&&(n[Y]&&t(n[Y]),function x(n){n&&B in n&&(x(n._next),t(n[B]+""),x(n._p))}(n)),(x&&x[_0x159f("0xf")]?x[_0x159f("0xf")]:x)+("\n"+e.join("\n"))[_0x159f("0xe")](ex,"")}function g(x,n){return x(n)}function T(x,n,t){var e=0,f=x._c
+if(x._s===L)for(x._s=n,x._v=t,n===P&&(V&&r(t)&&(t.longStack=y(t,x)),rx(x));e<f;)ix(x,x[e++])
+return x}function b(x,n){if(n===x&&n)return T(x,P,a(Q)),x
+if(n!==M&&(f(n)||e(n))){var t=u(S)(n)
+if(t===nx)return T(x,P,t.e),x
+f(t)?(V&&d(n)&&(x[_0x159f("0x10")]=n),d(n)?j(x,n,t):fx[_0x159f("0x11")](function(){j(x,n,t)})):T(x,F,n)}else T(x,F,n)
+return x}function S(x){return x[_0x159f("0x12")]}function j(x,n,t){var e=u(t,n)(function(t){n&&(n=M,b(x,t))},function(t){n&&(n=M,T(x,P,t))})
+e===nx&&n&&(T(x,P,e.e),n=M)}var U,I,k,M=null,C=_0x159f("0x13")==typeof window,D=C?window:n,E=D.process,R=D.console,V=!1,H=Array,O=Error,P=1,F=2,L=3,A=_0x159f("0x14"),N=_0x159f("0x15"),q=_0x159f("0x16"),X=A+"("+q+")",J=_0x159f("0x17"),z=_0x159f("0x18"),B="_pt",Y=_0x159f("0x19"),W=_0x159f("0x1a"),G=_0x159f("0x1b"),K="\nFrom previous ",Q=_0x159f("0x1c"),Z=_0x159f("0x1d"),$=_0x159f("0x1e"),xx="unhandledRejection",nx={e:M},tx=function(){},ex=/^.+\/node_modules\/yaku\/.+\n?/gm,fx=x[_0x159f("0x0")]=function(x){var n,t=this
+if(!e(t)||t._s!==U)throw a(W)
+if(t._s=L,V&&(t[B]=l()),x!==tx){if(!f(x))throw a(G);(n=u(x)(w(t,F),w(t,P)))===nx&&T(t,P,n.e)}}
+fx[_0x159f("0x1f")]=fx,function(x,n){for(var t in n)x[t]=n[t]}(fx[_0x159f("0x20")],{then:function(x,n){if(void 0===this._s)throw a()
+return h(this,p(fx[_0x159f("0x21")](this,fx)),x,n)},catch:function(x){return this[_0x159f("0x12")](U,x)},finally:function(x){function n(n){return fx[_0x159f("0x22")](x()).then(function(){return n})}return this[_0x159f("0x12")](n,n)},_c:0,_p:M}),fx[_0x159f("0x22")]=function(x){return d(x)?x:b(p(this),x)},fx.reject=function(x){return T(p(this),P,x)},fx[_0x159f("0x23")]=function(x){var n=this,t=p(n),e=function(x){T(t,F,x)},f=function(x){T(t,P,x)},i=u(s)(x,function(x){n[_0x159f("0x22")](x).then(e,f)})
+return i===nx?n[_0x159f("0x24")](i.e):t},fx[_0x159f("0x25")]=function(x){function n(x){T(f,P,x)}var t,e=this,f=p(e),i=[]
+return t=u(s)(x,function(x,r){e.resolve(x)[_0x159f("0x12")](function(x){i[r]=x,--t||T(f,F,i)},n)}),t===nx?e[_0x159f("0x24")](t.e):(t||T(f,F,[]),f)},fx[_0x159f("0x14")]=D[A]||{},u(function(){Object.defineProperty(fx,t(),{get:function(){return this}})})(),fx[_0x159f("0x21")]=function(x,n){var e=x[_0x159f("0x26")]
+return e?e[t()]||n:n},fx[_0x159f("0x27")]=function(x,n){R&&R[_0x159f("0x28")](Z,V?n[_0x159f("0x29")]:y(x,n))},fx[_0x159f("0x1e")]=tx,fx[_0x159f("0x2a")]=function(){V=!0},fx[_0x159f("0x11")]=C?function(x){setTimeout(x)}:E.nextTick,fx._s=1
+var ix=c(999,function(x,n){var t,e
+return e=x._s!==P?n._onFulfilled:n[_0x159f("0x2b")],e===U?void T(n,x._s,x._v):(t=u(g)(e,x._v),t===nx?void T(n,P,t.e):void b(n,t))}),rx=c(9,function(x){m(x)||(x[z]=1,v(xx,x))})}()})[_0x159f("0x1")](n,function(){return this}())}})
+var TS=function(){function x(){}function n(x){if(t(_0x159f("0x3c"),{visitorResponse:x}),null!=x.error_code&&""!=x[_0x159f("0x3d")])e("","","","",{SubType:_0x159f("0x3e"),visitorResponse:x,callType:4}),console[_0x159f("0x3f")](_0x159f("0x40")),console[_0x159f("0x3f")](x)
+else{null!=o(_0x159f("0x34"))&&""!=o("vstr")?(t("ReturningVisitor",{callType:1,HitType:x[_0x159f("0x41")]}),e(x.visitor_uid,x[_0x159f("0x42")],x[_0x159f("0x43")],o(_0x159f("0x31")),{callType:1,HitType:x[_0x159f("0x41")]})):null!=o("vstr")&&""!=o(_0x159f("0x34"))||null!=o(_0x159f("0x35"))&&""!=o(_0x159f("0x35"))?(t("Exceptional case of cookie",{callType:3,HitType:x.hittype}),e(x[_0x159f("0x45")],x[_0x159f("0x42")],x[_0x159f("0x43")],o(_0x159f("0x31")),{callType:3,HitType:x[_0x159f("0x41")]})):(t(_0x159f("0x44"),{callType:2,HitType:x[_0x159f("0x41")]}),e(x.visitor_uid,x[_0x159f("0x42")],x.user_uid,o(_0x159f("0x31")),{callType:2,HitType:x[_0x159f("0x41")]}))
+var n=location[_0x159f("0x32")],f=p(n),i=new Date
+i.setFullYear(i[_0x159f("0x46")]()+5),""==o(_0x159f("0x34"))&&null!=x.visitor_uid&&_(_0x159f("0x34"),x.visitor_uid,i[_0x159f("0x47")](),f),null!=x[_0x159f("0x42")]&&_("vsuid",x.visit_uid,"",f)
+var r=x[_0x159f("0x48")]
+if(""==o(_0x159f("0x33"))&&null!=r&&r>0&&_(_0x159f("0x33"),r,i[_0x159f("0x47")](),f),null!=x[_0x159f("0x49")]){var u=x[_0x159f("0x49")][_0x159f("0xe")](/\s*,\s*/g,",")
+_(_0x159f("0x49"),u,"",f)}_(_0x159f("0x36"),x[_0x159f("0x36")],i[_0x159f("0x47")](),f),null!=x[_0x159f("0x4a")]&&_(_0x159f("0x4a"),x[_0x159f("0x4a")],"",f),t(_0x159f("0x4b"))}}function t(n,t){void 0!=t&&null!=t||(t={}),t.visitor_js_version=window.TS[_0x159f("0x4c")],window[_0x159f("0x4d")]&&MetricsUtil[_0x159f("0x4e")]?(i(),MetricsUtil[_0x159f("0x4f")](n,t)):x[_0x159f("0x2c")][_0x159f("0xd")]({message:n,addProps:t})}function e(x,n,t,e,i){var o={VisitUID:n,VisitorUID:x,UserUID:t,VsSessionUID:e}
+if(window.MetricsUtil&&MetricsUtil[_0x159f("0x4e")])f(o,i,0)
+else var _=r(function(x){f(o,i,x)&&clearInterval(_)},200,5)}function f(x,n,t){void 0!=n&&null!=n||(n={}),n[_0x159f("0x50")]=t,n[_0x159f("0x51")]=window.TS[_0x159f("0x4c")]
+var e=![]
+if(window[_0x159f("0x4d")]&&MetricsUtil.isInitDone){i(),e=!![]
+MetricsUtil[_0x159f("0x52")](x,n)}return e}function i(){if(x[_0x159f("0x2c")][_0x159f("0x5")]>0){var n=x[_0x159f("0x2c")]
+x[_0x159f("0x2c")]=[]
+for(var t=0;t<n[_0x159f("0x5")];t++){var e=window.SplashEventStream?new(SplashEventStream[_0x159f("0x53")][_0x159f("0x54")]):new(BoldEventStream.ClassInitializers[_0x159f("0x54")])
+MetricsUtil[_0x159f("0x4f")](n[t][_0x159f("0x55")],n[t].addProps,e)}}}function r(x,n,t){var e=0,f=window[_0x159f("0x56")](function(n){x(++e,n),e===t&&window[_0x159f("0x57")](f)},n)
+return f}function o(x){var n=document[_0x159f("0x58")][_0x159f("0x59")](_0x159f("0x5a")+x+_0x159f("0x5b"))
+return n?n[_0x159f("0x5c")]():""}function _(x,n,t,e){var f=x+"="+n+";path=/; domain=."+e+";"
+""!=t&&(f+=_0x159f("0x5d")+t+";"),f+=_0x159f("0x5e"),document[_0x159f("0x58")]=f}function u(x,n,t,e){return new(window[_0x159f("0x3")])(function(f,i){void 0===e&&(e=null)
+try{var r=new(window[_0x159f("0x5f")]||ActiveXObject)(_0x159f("0x60"))
+r[_0x159f("0x61")](_0x159f("0x62"),x,1),r[_0x159f("0x63")]("Accept",_0x159f("0x64")),r[_0x159f("0x63")](_0x159f("0x65"),_0x159f("0x66")),r[_0x159f("0x67")]=function(){if(r.readyState>3&&(r.responseText&&n&&n(JSON.parse(r[_0x159f("0x68")])),void 0!==f))return f(201==r.status?!![]:![])},r[_0x159f("0x69")](t)}catch(x){}})}function c(){var x=![],n=s(5)
+return(document[_0x159f("0x58")]=n+_0x159f("0x6a"))[_0x159f("0x6b")].call(document[_0x159f("0x58")],n)>-1&&(document[_0x159f("0x58")]=n+_0x159f("0x6c")+new Date(98,1)[_0x159f("0x47")]()+";",x=!![]),x}function s(x){if(1==x)return"v"
+if(x<1)return null
+x-=2
+for(var n="vs",t="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",e=t[_0x159f("0x5")],f=0;f<x;f++)n+=t[_0x159f("0x6d")](Math[_0x159f("0x6e")](Math[_0x159f("0x6f")]()*e))
+return n}function a(x){var n=new RegExp("[?&]"+x+_0x159f("0x70"),"i"),t=n.exec(window[_0x159f("0x37")][_0x159f("0x71")])
+return t?t[1]:null}function l(){var x=o(_0x159f("0x36")),n=[a(_0x159f("0x72")),a("utm_medium"),a(_0x159f("0x73")),a(_0x159f("0x74")),a(_0x159f("0x75"))],t=n.join("|")
+return""===x||t===_0x159f("0x76")?![]:0===x.substring(x[_0x159f("0x77")]("#")+1)[_0x159f("0x78")](t)?![]:!![]}function v(x,n){if(null==x||null==n)return![]
+var t=p(x),e=o(_0x159f("0x36")),f=null!=e?e[_0x159f("0x79")]("#"):[],i=f[_0x159f("0x5")]>=6?p(f[f[_0x159f("0x5")]-2]):""
+return null!=i&&""!==i&&i===t?![]:t===p(n)}function d(){var x=![]
+try{var n=window[_0x159f("0x7a")][_0x159f("0x7b")](_0x159f("0x7c"))
+x=n[0][_0x159f("0x7d")]===_0x159f("0x7e")?!![]:![]}catch(n){x=![]}return x}function p(x){return(x.match(new RegExp("\\.","g"))||[])[_0x159f("0x5")]>=2?x.slice(x[_0x159f("0x6b")](".")+1):x}function w(){var x=o(_0x159f("0x7f"))
+if(null!=x&&""!==x){return JSON[_0x159f("0x80")](x)[_0x159f("0x81")][_0x159f("0x82")]}return null}function h(){var x=(new Date)[_0x159f("0x83")]()
+return _0x159f("0x84")[_0x159f("0xe")](/[xy]/g,function(n){var t=(x+16*Math[_0x159f("0x6f")]())%16|0
+return x=Math[_0x159f("0x6e")](x/16),("x"==n?t:3&t|8)[_0x159f("0x85")](16)})}return x.version="v7_02",x[_0x159f("0x2c")]=[],x.Track=function(x,f,i,r,s){void 0===i&&(i=!![]),void 0===r&&(r=!![]),void 0===s&&(s=![])
+var a
+if(s===!![])a=_0x159f("0x2d")
+else{var m=location.hostname,y=p(m)
+a=_0x159f("0x2e")+y+_0x159f("0x2f")}var g=document[_0x159f("0x30")]?document[_0x159f("0x30")]:null,T=o(_0x159f("0x31"))
+""==T&&(T=h(),_(_0x159f("0x31"),T,"",p(location[_0x159f("0x32")])))
+var b={referrer_id:o(_0x159f("0x33")),visitor_uid:o(_0x159f("0x34")),referrer:g,product_cd:x,visit_uid:o(_0x159f("0x35")),include_geolocation:r,include_useragent:i,vsutms:o(_0x159f("0x36")),request_url:window[_0x159f("0x37")][_0x159f("0x38")],cookies_enabled:c(),vsintentcd:o("vsintentcd"),user_uid:w(),portal_cd:f,vssessionuid:T},S=!![]
+return void 0!==x&&null!==x&&x[_0x159f("0x8")]()===_0x159f("0x39")||(d()?S=![]:!v(g,window[_0x159f("0x37")][_0x159f("0x38")])||""===o("vsuid")||i||r||(S=l())),S?(t("VisitorCallInitiated",{DataObj:b}),u(a,n,JSON[_0x159f("0x3a")](b),null)):(e(b.visitor_uid,b.visit_uid,b.user_uid,b.vssessionuid,{callType:0}),t(_0x159f("0x3b"),{callType:0,DataObj:b}),new window.Promise(function(x,n){if(void 0!==x)return x(!![])}))},x}()
